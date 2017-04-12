cf356ffc8b9969117d628f8bd1b89e693103ec1c
raviteja4c3 <raviteja4c3@gmail.com>
2017-04-12 17:06:48 +0530

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -9,7 +9,8 @@ public class MainApp {
 
 	public static void main(String[] args) {
 	ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-	
+			
+	//MyService myService = new MyService();	
 	MyService myService = context.getBean("PrintService",MyService.class);
 	//MyServiceImpl myService = context.getBean("PrintService",MyServiceImpl.class);
 	//MyServiceImpl myService = (MyServiceImpl)context.getBean("PrintService");
@@ -19,6 +20,8 @@ public class MainApp {
 	
 }
 /*
+If you observe Line 13, it is commented, where object is crated manually. Dependecy Injection is to let Spring create the object. 
+So the next three lines will do that.
 In the above example, getBean("string", <class>) method retruns the object of type mentioned in <class>
 Another method getBean("string") method returns the the object but it won't type cast to any particular calss,
 which we have to manually do (in line 15 we did that, type casting return object of method to 'MyServiceImpl' type).
