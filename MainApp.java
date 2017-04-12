6eef490afeb19c6ff6079ee47f0bc7bd5647fa06
raviteja4c3 <raviteja4c3@gmail.com>
2017-04-12 17:03:51 +0530

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -9,8 +9,18 @@ public class MainApp {
 
 	public static void main(String[] args) {
 	ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+	
 	MyService myService = context.getBean("PrintService",MyService.class);
+	//MyServiceImpl myService = context.getBean("PrintService",MyServiceImpl.class);
+	//MyServiceImpl myService = (MyServiceImpl)context.getBean("PrintService");
+		
 	myService.printMe();		
 	}
 	
 }
+/*
+In the above example, getBean("string", <class>) method retruns the object of type mentioned in <class>
+Another method getBean("string") method returns the the object but it won't type cast to any particular calss,
+which we have to manually do (in line 15 we did that, type casting return object of method to 'MyServiceImpl' type).
+So you can use any of the above two methods, both are same.
+*/
